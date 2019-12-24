27b7605093736a9cc4890a06802b4464e9567370
Yarigin <57716868+Yarigin@users.noreply.github.com>
2019-12-24 20:42:52 +0500

    fix.update

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,44 @@
 let popup = document.querySelector(".popup");
 let btnOn = document.querySelector(".popup-btn");
 let btnOff = document.querySelector(".popup-close");
-btnOn.onclick = function(){
-  popup.classList.remove("popup-off");
+
+let form = popup.querySelector("form");
+let name = popup.querySelector("[name=name]");
+let email = popup.querySelector("[name=email]");
+
+let requiredName = popup.querySelector(".input-name");
+let requiredEmail =popup.querySelector(".input-email");
+
+btnOn.addEventListener("click", function(evt) {
+  evt.preventDefault();
   popup.classList.add("popup-on");
-};
-btnOff.onclick = function(){
+  name.focus();
+});
+btnOff.addEventListener("click", function(evt){
+  evt.preventDefault();
   popup.classList.remove("popup-on");
-  popup.classList.add("popup-off");
-};
+  requiredName.classList.remove("input-required");
+});
+
+form.addEventListener("submit", function (evt) {
+    if (!name.value || !email.value) {
+      evt.preventDefault();
+      requiredName.classList.add("input-required");
+      requiredEmail.classList.add("input-required");
+} else {
+      localStorage.setItem("name", name.value);
+      localStorage.setItem("email", email.value);
+    }
+  });
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("popup-on")) {
+        popup.classList.remove("popup-on");
+      }
+    }
+  });
+
+
 
